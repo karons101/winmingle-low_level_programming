bde51a8c3e5fa1a8cd910281fd1ddedb78c66a28
karons101 <karonstical@gmail.com>
2025-10-23 06:27:39 +0100

    Add program that prints message using printf

---
+++ 5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Steps taken in this multi line comments:
+   Here is the  main - Entry point
+ *
+ * Description: Prints the string "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line using printf().
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* printf requires the explicit newline character \n */
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    
+    return (0);
+}
