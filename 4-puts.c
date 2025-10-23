f2d2996586ba4fc45fd4d02be87459ddd5ac9596
karons101 <karonstical@gmail.com>
2025-10-23 06:27:14 +0100

    Add program that prints message using puts

---
+++ 4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*  I found out The puts function prints the string and appends a newline character */
+    puts("\"Programming is like building a multilingual puzzle");
+    
+    return (0);
+}
